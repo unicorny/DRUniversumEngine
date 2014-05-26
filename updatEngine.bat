527c835f110315d070abc6c2968ace304ba073e6
Dario Rekowski Alesandra <dario.rekowski@gmx.de>
2014-05-26 21:50:23 +0200

    added first files for universumLIb

---
+++ updatEngine.bat
@@ -0,0 +1,12 @@
+xcopy /Y Z:\Engine2\dependencies\bin\libDRCore2.dll .
+xcopy /Y Z:\Engine2\dependencies\bin\libDRCore2.dll import\bin\
+
+xcopy /Y Z:\Engine2\dependencies\bin\Debug\DRCore2.dll .
+xcopy /Y Z:\Engine2\dependencies\bin\Debug\DRCore2.dll import\bin\
+
+xcopy /Y Z:\Engine2\dependencies\bin\Debug\DRCore2.dll Debug\
+xcopy /Y Z:\Engine2\dependencies\bin\Release\DRCore2.dll Release\
+
+xcopy /Y /s Z:\Engine2\dependencies\Core2\include\* import\include\Core2\
+xcopy /Y /s Z:\Engine2\dependencies\lib\Debug\DRCore2.lib import\lib\
+
