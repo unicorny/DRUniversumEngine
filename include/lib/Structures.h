48348068fe1941430a081a0d27d4b57eb90bf2c7
Dario <dario.rekowski@gmx.de>
2016-05-27 11:15:38 +0200

    fix compile bug

--- include/lib/Structures.h
+++ include/lib/Structures.h
@@ -19,7 +19,6 @@
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
 *                                                                         *
 ***************************************************************************/
- ***************************************************************************/
 
 /**
  * \author Dario Rekowski copied from: http://fara.cs.uni-potsdam.de/~kaufmann/?page=GenCppFaqs&faq=Singleton
